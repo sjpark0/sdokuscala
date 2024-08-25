9773b29a30211b534f862243113c935d553ed713
sjpark <sjpark0@gmail.com>
2024-08-25 15:12:49 +0900

    fast solver추가 및 시간측정 추가

--- src/main/scala/FastSolver.scala
+++ src/main/scala/FastSolver.scala
@@ -8,21 +8,42 @@ object FastSolver extends Solver{
     result._2
   }
   
-  def SolveSdoku1(sdoku : SdokuCell, emptyList : List[Coord]) : (Int, SdokuCell) = {
-   if emptyList.isEmpty then (1, sdoku) else{
+  def RemoveTrivialCell(sdoku : SdokuCell, emptyList: List[Coord], res : List[Coord]) : (Option[SdokuCell], List[Coord]) = {
+    if emptyList.isEmpty then (Some(sdoku), res) 
+    else{
       val sdokuTemp = sdoku.map(_.clone)
       val availList = Solver.GetAvailableNumber(sdokuTemp, emptyList.head._1, emptyList.head._2)
-      if availList.isEmpty then (0, Array[Array[Int]]()) else{
-        availList.foldLeft(0, Array[Array[Int]]()) ((b, a) => {
-          sdokuTemp(emptyList.head._1)(emptyList.head._2) = a
-          val tmp = SolveSdoku1(sdokuTemp, emptyList.tail)
-          if tmp._1 == 1 then tmp else b
-          /*if b._1 + tmp._1 != 1 then (b._1 + tmp._1, Array[Array[Int]]()) else{
-            if b._1 == 1 then b else tmp
-          }*/
-        })
+      if availList.isEmpty then (None, Nil)
+      else if availList.length == 1 then {
+        sdokuTemp(emptyList.head._1)(emptyList.head._2) = availList.head
+        RemoveTrivialCell(sdokuTemp, res ::: emptyList.tail, Nil)
+      }
+      else{
+        RemoveTrivialCell(sdokuTemp, emptyList.tail, res ::: List(emptyList.head))
       }
     }
+    
+  }
+  def SolveSdoku1(sdoku : SdokuCell, emptyList : List[Coord]) : (Int, SdokuCell) = {
+    //val sdokuTemp = sdoku.map(_.clone)
+    val (sdokuTemp, emptyListTemp) = RemoveTrivialCell(sdoku, emptyList, List())
+    sdokuTemp match{
+      case Some(s) => {
+        if emptyListTemp.isEmpty then (1, s) 
+        else{
+          val availList = Solver.GetAvailableNumber(s, emptyListTemp.head._1, emptyListTemp.head._2)
+          if availList.isEmpty then (0, Array[Array[Int]]()) else{
+            availList.foldLeft(0, Array[Array[Int]]()) ((b, a) => {
+              s(emptyListTemp.head._1)(emptyListTemp.head._2) = a
+              val tmp = SolveSdoku1(s, emptyListTemp.tail)
+              if tmp._1 == 1 then tmp else b
+            })
+          }
+        }
+      }
+      case None => (0, Array[Array[Int]]())
+    }
+    
   }
   
 }
--- src/main/scala/Sdoku.scala
+++ src/main/scala/Sdoku.scala
@@ -14,15 +14,27 @@ object Main extends App:
 
   Solver.PrintSdoku(sdoku)
   Solver.PrintSdoku(sdokuTemp)*/
+  var s = System.currentTimeMillis
   val solved = NaiveSolver.SolveSdoku(sdoku)
+  var e = System.currentTimeMillis
+  println("NaiveSolver : [elapsedTime]: " + ((e - s)) + " ms")
+
   Solver.PrintSdoku(solved)
-  
+  s = System.currentTimeMillis
   val solved1 = FastSolver.SolveSdoku(sdoku)
+  e = System.currentTimeMillis
+  println("FastSolver : [elapsedTime]: " + ((e - s)) + " ms")
+
   Solver.PrintSdoku(solved1)
   /*sdoku(0)(0) = 1
   sdoku(0)(2) = 8
   sdoku(0)(3) = 3
   println(Solver.GetAvailableNumber(sdoku, 0, 4))*/
+  s = System.currentTimeMillis
   val solved2 = FastSolver1.SolveSdoku(sdoku)
+  e = System.currentTimeMillis
+  println("FastSolver1 : [elapsedTime]: " + ((e - s)) + " ms")
+
   Solver.PrintSdoku(solved2)
+  
 end Main
